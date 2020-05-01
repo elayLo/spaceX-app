b82b5147c0ffcc078a3a666a02bfaa50a9ac4f9c
a.sergeenkov <a.sergeenkov@int.robotbull.com>
2020-05-01 16:31:21 +0300

    Show data from api

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+import Launches from "./components/Launches";
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+})
+
+function App() {
+  return (
+      <ApolloProvider client={client}>
+          <div className="App">
+              <div className="logo">
+                  <h1>SpaceX</h1>
+              </div>
+              <Launches/>
+          </div>
+      </ApolloProvider>
+
+  );
+}
+
+export default App;
