e918d753e7819e8f072f16b0879b0183aef30178
Alexander Sergeenkov <40717380+Alexander-Sergeenkov@users.noreply.github.com>
2020-05-02 02:05:37 +0300

    Final commit

--- client/src/components/Launches/LaunchItem.js
+++ client/src/components/Launches/LaunchItem.js
@@ -2,15 +2,18 @@ import React from "react";
 import gql from 'graphql-tag';
 import {Query, useQuery} from 'react-apollo';
 import Moment from "react-moment";
+import {Link} from 'react-router-dom';
 
 
 export default function LaunchItem(props) {
     return (
         <div className={props.launch_success ? "launch launch_success" : "launch launch_failed"}>
-            <p>{props.mission_name}</p>
-            {props.launch_success ? <span>Success</span> : <span>Failed</span>}
-            <p className="launch__date">{props.launch_date_local}</p>
-            <img src={require('./../../assets/left-and-right-arrows.svg')} alt=""/>
+            <Link to={`/launch/${props.flight_number}`}>
+                <p className="launch__name">{props.mission_name}</p>
+                {props.launch_success ? <span>Success</span> : <span>Failed</span>}
+                <p className="launch__date"><Moment format="HH:mm DD-MM-YYYY">{props.launch_date_local}</Moment></p>
+                <img src={require('./../../assets/left-and-right-arrows.svg')} alt=""/>
+            </Link>
         </div>
     )
 }
